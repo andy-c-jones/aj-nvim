ad4d62e4c41a3540244588690cbf0d93c627975c
Andrew 'AJ' Jones <aj@darkpeak.dev>
2025-07-21 23:07:39 +0100

    Adds gitsigns

--- init.lua
+++ init.lua
@@ -353,6 +353,89 @@ local plugins = {
     end,
   },
 
+  -- Git signs
+  {
+    "lewis6991/gitsigns.nvim",
+    config = function()
+      require("gitsigns").setup({
+        signs = {
+          add          = { text = '│' },
+          change       = { text = '│' },
+          delete       = { text = '_' },
+          topdelete    = { text = '‾' },
+          changedelete = { text = '~' },
+          untracked    = { text = '┆' },
+        },
+        signcolumn = true,
+        numhl = false,
+        linehl = false,
+        word_diff = false,
+        watch_gitdir = {
+          follow_files = true
+        },
+        attach_to_untracked = true,
+        current_line_blame = false,
+        current_line_blame_opts = {
+          virt_text = true,
+          virt_text_pos = 'eol',
+          delay = 1000,
+          ignore_whitespace = false,
+        },
+        sign_priority = 6,
+        update_debounce = 100,
+        status_formatter = nil,
+        max_file_length = 40000,
+        preview_config = {
+          border = 'single',
+          style = 'minimal',
+          relative = 'cursor',
+          row = 0,
+          col = 1
+        },
+        on_attach = function(bufnr)
+          local gs = package.loaded.gitsigns
+
+          local function map(mode, l, r, opts)
+            opts = opts or {}
+            opts.buffer = bufnr
+            vim.keymap.set(mode, l, r, opts)
+          end
+
+          -- Navigation
+          map('n', ']c', function()
+            if vim.wo.diff then return ']c' end
+            vim.schedule(function() gs.next_hunk() end)
+            return '<Ignore>'
+          end, {expr=true})
+
+          map('n', '[c', function()
+            if vim.wo.diff then return '[c' end
+            vim.schedule(function() gs.prev_hunk() end)
+            return '<Ignore>'
+          end, {expr=true})
+
+          -- Actions
+          map('n', '<leader>hs', gs.stage_hunk)
+          map('n', '<leader>hr', gs.reset_hunk)
+          map('v', '<leader>hs', function() gs.stage_hunk {vim.fn.line('.'), vim.fn.line('v')} end)
+          map('v', '<leader>hr', function() gs.reset_hunk {vim.fn.line('.'), vim.fn.line('v')} end)
+          map('n', '<leader>hS', gs.stage_buffer)
+          map('n', '<leader>hu', gs.undo_stage_hunk)
+          map('n', '<leader>hR', gs.reset_buffer)
+          map('n', '<leader>hp', gs.preview_hunk)
+          map('n', '<leader>hb', function() gs.blame_line{full=true} end)
+          map('n', '<leader>tb', gs.toggle_current_line_blame)
+          map('n', '<leader>hd', gs.diffthis)
+          map('n', '<leader>hD', function() gs.diffthis('~') end)
+          map('n', '<leader>td', gs.toggle_deleted)
+
+          -- Text object
+          map({'o', 'x'}, 'ih', ':<C-U>Gitsigns select_hunk<CR>')
+        end
+      })
+    end,
+  },
+
   -- GitHub Copilot
   {
     "github/copilot.vim",
@@ -745,6 +828,25 @@ local function show_cheatsheet()
     "   <leader>tS         Toggle test summary",
     "   <leader>tw         Toggle test watch mode",
     "   <leader>fm         Format file",
+    "",
+    "🤖 COPILOT",
+    "   <C-J>              Accept suggestion",
+    "   <C-L>              Accept word",
+    "   <C-K>              Next suggestion",
+    "   <C-H>              Previous suggestion",
+    "",
+    "🔧 GIT (Gitsigns)",
+    "   ]c / [c            Next/Previous hunk",
+    "   <leader>hs         Stage hunk",
+    "   <leader>hr         Reset hunk",
+    "   <leader>hS         Stage buffer",
+    "   <leader>hu         Undo stage hunk",
+    "   <leader>hR         Reset buffer",
+    "   <leader>hp         Preview hunk",
+    "   <leader>hb         Show blame line",
+    "   <leader>tb         Toggle line blame",
+    "   <leader>hd         Diff this",
+    "   <leader>td         Toggle deleted",
     "",
     "❓ HELP",
     "   <leader>cc         Show this cheatsheet",
